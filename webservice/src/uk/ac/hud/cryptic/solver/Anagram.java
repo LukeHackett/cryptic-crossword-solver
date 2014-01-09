77535b592b4225c4acc4c7d84d9a1070c6647c9e
Stuart Leader <slug56@gmail.com>
2014-01-09 10:52:02 +0000

    Comment of test clue's solution

--- webservice/src/uk/ac/hud/cryptic/solver/Anagram.java
+++ webservice/src/uk/ac/hud/cryptic/solver/Anagram.java
@@ -16,7 +16,7 @@ public class Anagram extends Solver {
 	public void run() {
 		Anagram a = new Anagram();
 		a.solve(new Clue("Manger apt to be shown transformed by star", "?????????")); // pentagram
-		a.solve(new Clue("Not straight, with future too unsettled", "???,??,????"));
+		a.solve(new Clue("Not straight, with future too unsettled", "???,??,????")); // out of true
 	}
 
 	public SolutionCollection solve(Clue c) {
