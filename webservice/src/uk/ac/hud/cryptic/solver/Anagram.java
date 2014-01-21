a34995b0604e3d3661bc46e4a4b77b31bc830e29
Stuart Leader <slug56@gmail.com>
2014-01-21 14:06:50 +0000

    Prep work for alternate Anagram implementation

--- webservice/src/uk/ac/hud/cryptic/solver/Anagram.java
+++ webservice/src/uk/ac/hud/cryptic/solver/Anagram.java
@@ -17,6 +17,9 @@ import uk.ac.hud.cryptic.core.SolutionPattern;
  */
 public class Anagram extends Solver {
 
+	// The potential solutions found
+	private SolutionCollection solutions;
+
 	/**
 	 * Default constructor for solver class
 	 * 
@@ -39,46 +42,48 @@ public class Anagram extends Solver {
 	 */
 	public static void main(String[] args) {
 		Anagram a = new Anagram();
-		a.testSolver(a, Type.ANAGRAM);
+		// a.testSolver(a, Type.ANAGRAM);
+		Clue c = new Clue("Manger apt to be shown transformed by star",
+				"?????????");
+		SolutionCollection sc = a.solve(c);
+		for (Solution s : sc) {
+			System.out.println(s);
+		}
 	}
-
-	private SolutionCollection solutions;
 
 	public SolutionCollection solve(Clue c) {
 		// Solution collection
 		solutions = new SolutionCollection();
 		// Get clue pattern
 		final SolutionPattern pattern = c.getPattern();
+		// Get length of full answer
+		int solutionLength = pattern.getTotalLength();
 
 		// Get clue with no punctuation
 		String fodder = c.getClueNoPunctuation(true);
 
 		// Clue length must be greater than solution length
-		if (fodder.length() < c.getPattern().getTotalLength()) {
+		if (fodder.length() < solutionLength) {
 			return solutions;
 		}
 
-		// Get length of full answer
-		int lengthOfFodder = c.getPattern().getTotalLength();
-
 		// Get words in clue
 		String[] words = c.getClueWords();
-		// Get length of words in clue
-		int[] wordLengths = new int[words.length];
 
 		// Get possible fodder
 		Collection<String> possibleFodder = getPossibleFodder(words,
-				wordLengths, lengthOfFodder);
+				solutionLength);
 
 		// For each potential fodder, find all potential solutions
 		for (String p : possibleFodder) {
 			char[] fodderEntry = p.toCharArray();
-			findPotential(fodderEntry, 0);
+			generateAnagrams(fodderEntry, 0);
 		}
 
 		// Remove risk of matching original words
 		solutions.removeAllStrings(Arrays.asList(c.getClueWords()));
 
+		// TODO Do we need this with the new method?
 		// Remove solutions which don't match the provided pattern
 		pattern.filterSolutions(solutions);
 
@@ -88,31 +93,45 @@ public class Anagram extends Solver {
 		return solutions;
 	}
 
+	/**
+	 * The solution should be composed of all the characters from one (or more)
+	 * words contained within the clue. In other words, the anagram solution
+	 * cannot be formed from characters that have been picked and chosen across
+	 * all the words of the clue. This method creates a list of possible
+	 * substrings of the entire clue, which match up with the length of the
+	 * solution.
+	 * 
+	 * @param words
+	 *            - all the words present in the clue
+	 * @param solutionLength
+	 *            - the target length of the solution
+	 * @return a collection of all terms present in the clue which match up with
+	 *         the length of the soluton
+	 */
 	public Collection<String> getPossibleFodder(String[] words,
-			int[] wordLengths, int lengthOfFodder) {
-		// Fill array with word lengths
-		for (int i = 0; i < words.length; i++) {
-			wordLengths[i] = words[i].length();
-		}
+			int solutionLength) {
 
 		// Structure for potential fodder
 		Collection<String> possibleFodder = new ArrayList<>();
 
-		// Find word lengths which add up to clue answer
-		for (int i = 0; i < wordLengths.length; i++) {
-			int length = wordLengths[i];
+		// For each word of the given clue
+		for (int i = 0; i < words.length; i++) {
+			// If a single word makes up the length of the fodder
+			if (words[i].length() == solutionLength) {
+				// Add single word
+				possibleFodder.add(words[i]);
+			} else {
+				// The length of the current word
+				int length = words[i].length();
+				// For each subsequent word to the current word
+				for (int j = i + 1; j < words.length; j++) {
+					// Add the length of the next word
+					length += words[j].length();
 
-			for (int j = i + 1; j < wordLengths.length; j++) {
-				// If a single word makes up the length of the fodder
-				if (words[i].length() == lengthOfFodder) {
-					// Add single word
-					possibleFodder.add(words[i]);
-				} else {
-					// Increment length of potential fodder by next word
-					length += wordLengths[j];
-
-					// If length is equal to the length of fodder
-					if (length == lengthOfFodder) {
+					// If length is equal to the length of fodder (or just
+					// above)
+					// TODO == is the general rule, not >=, but isn't set in concrete
+					if (length == solutionLength) {
 						// Add words within potential fodder to list
 						String possible = "";
 
@@ -120,9 +139,9 @@ public class Anagram extends Solver {
 							possible += words[x];
 						}
 						possibleFodder.add(possible);
-					}
-					// If length has exceeded, stop
-					else if (length > lengthOfFodder) {
+						break;
+					} else if (length > solutionLength) {
+						// If length has exceeded, stop
 						break;
 					}
 				}
@@ -141,10 +160,10 @@ public class Anagram extends Solver {
 		solutions.add(new Solution(swapped));
 	}
 
-	public void findPotential(char[] fodderEntry, int start) {
+	public void generateAnagrams(char[] fodderEntry, int start) {
 		for (int i = start; i < fodderEntry.length; i++) {
 			swap(fodderEntry, start, i);
-			findPotential(fodderEntry, start + 1);
+			generateAnagrams(fodderEntry, start + 1);
 			swap(fodderEntry, start, i);
 		}
 	}
