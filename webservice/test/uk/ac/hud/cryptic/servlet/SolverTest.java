fa04deffbb70faa418521fe023282a751158a403
Luke Hackett <luke.hackett@live.co.uk>
2014-01-22 16:25:53 +0000

    SovlerTest now tests all three return types - HTML, XML and JSON

--- webservice/test/uk/ac/hud/cryptic/servlet/SolverTest.java
+++ webservice/test/uk/ac/hud/cryptic/servlet/SolverTest.java
@@ -12,11 +12,16 @@ import java.net.URISyntaxException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -42,6 +47,15 @@ public class SolverTest {
 	private static String path;
 	// Closeable client used to protected against memory leaks
 	private static CloseableHttpClient client;
+
+	//
+	public enum HttpVerb {
+		GET, POST, PUT, DELETE
+	}
+
+	public enum HttpResponse {
+		HTML, XML, JSON
+	}
 
 	/**
 	 * Initialises a number of base variables
@@ -76,151 +90,372 @@ public class SolverTest {
 
 	/**
 	 * This method will test to ensure that the server does not try to process a
-	 * bad request. This method will attempt to post three blank variables.
+	 * bad request. This method will attempt to post three blank variables, and
+	 * will test for HTML, XML and JSON responses.
 	 */
 	@Test
 	public void testNoParameters() {
+		// Input Values
 		String clue = "";
 		String solution = "";
 		String pattern = "";
 		int statusCode = HttpServletResponse.SC_BAD_REQUEST;
-		String errorMessage = "Please enter a clue to solve.";
+		String message = "Please enter a clue to solve.";
 
-		// Run the bad request test
-		inputBadRequest(clue, solution, pattern, statusCode, errorMessage);
+		// Run the Test
+		doTest(clue, solution, pattern, statusCode, message);
 	}
 
 	/**
 	 * This method will test to ensure that the server does not try to process a
-	 * bad request. This method will attempt to only post the clue.
+	 * bad request. This method will attempt to only post the clue, and will
+	 * test for HTML, XML and JSON responses.
 	 */
 	@Test
 	public void testClueParameterOnly() {
+		// Input Values
 		String clue = "Found ermine, deer hides damaged";
 		String solution = "";
 		String pattern = "";
+		// Expected HTML status code
 		int statusCode = HttpServletResponse.SC_BAD_REQUEST;
-		String errorMessage = "Please enter a valid solution format.";
+		// Expected output message
+		String message = "Please enter a valid solution format.";
 
-		// Run the bad request test
-		inputBadRequest(clue, solution, pattern, statusCode, errorMessage);
+		// Run the test
+		doTest(clue, solution, pattern, statusCode, message);
 	}
 
 	/**
 	 * This method will test to ensure that the server does not try to process a
 	 * bad request. This method will attempt to only post the clue and the
-	 * solution format.
+	 * solution format, and will test for HTML, XML and JSON responses.
 	 */
 	@Test
 	public void testClueSolutionParameterOnly() {
+		// Input Values
 		String clue = "Found ermine, deer hides damaged";
 		String solution = "10";
 		String pattern = "";
+		// Expected HTML status code
 		int statusCode = HttpServletResponse.SC_BAD_REQUEST;
-		String errorMessage = "Please enter a valid pattern format.";
+		// Expected output message
+		String message = "Please enter a valid pattern format.";
 
-		// Run the bad request test
-		inputBadRequest(clue, solution, pattern, statusCode, errorMessage);
+		// Run the test
+		doTest(clue, solution, pattern, statusCode, message);
 	}
 
 	/**
 	 * This method will test to ensure that the server does not try to process a
 	 * bad request. This method will attempt to only post the clue and the
-	 * pattern format.
+	 * pattern format, and will test for HTML, XML and JSON responses.
 	 */
 	@Test
 	public void testCluePatternParameterOnly() {
+		// Input Values
 		String clue = "Found ermine, deer hides damaged";
 		String solution = "";
 		String pattern = "??????????";
+		// Expected HTML status code
 		int statusCode = HttpServletResponse.SC_BAD_REQUEST;
-		String errorMessage = "Please enter a valid solution format.";
+		// Expected output message
+		String message = "Please enter a valid solution format.";
 
-		// Run the bad request test
-		inputBadRequest(clue, solution, pattern, statusCode, errorMessage);
+		// Run the test
+		doTest(clue, solution, pattern, statusCode, message);
 	}
 
 	/**
 	 * This method will test to ensure that the server does not try to process a
 	 * bad request. This method will attempt to only post the solution format
-	 * and the pattern format.
+	 * and the pattern format, and will test for HTML, XML and JSON responses.
 	 */
 	@Test
 	public void testSolutionPatternParameterOnly() {
+		// Input Values
 		String clue = "";
 		String solution = "10";
 		String pattern = "??????????";
+		// Expected HTML status code
 		int statusCode = HttpServletResponse.SC_BAD_REQUEST;
-		String errorMessage = "Please enter a clue to solve.";
+		// Expected output message
+		String message = "Please enter a clue to solve.";
 
-		// Run the bad request test
-		inputBadRequest(clue, solution, pattern, statusCode, errorMessage);
+		// Run the test
+		doTest(clue, solution, pattern, statusCode, message);
 	}
 
 	/**
 	 * This method will test to ensure that the server does not try to process a
-	 * bad request. This method will attempt to only post the solution format.
+	 * bad request. This method will attempt to only post the solution format,
+	 * and will test for HTML, XML and JSON responses.
 	 */
 	@Test
 	public void testSolutionParameterOnly() {
+		// Input Values
 		String clue = "";
 		String solution = "10";
 		String pattern = "";
+		// Expected HTML status code
 		int statusCode = HttpServletResponse.SC_BAD_REQUEST;
-		String errorMessage = "Please enter a clue to solve.";
+		// Expected output message
+		String message = "Please enter a clue to solve.";
 
-		// Run the bad request test
-		inputBadRequest(clue, solution, pattern, statusCode, errorMessage);
+		// Run the test
+		doTest(clue, solution, pattern, statusCode, message);
 	}
 
 	/**
 	 * This method will test to ensure that the server does not try to process a
-	 * bad request. This method will attempt to only post the pattern format.
+	 * bad request. This method will attempt to only post the pattern format,
+	 * and will test for HTML, XML and JSON responses.
 	 */
 	@Test
 	public void testPatternParameterOnly() {
+		// Input Values
 		String clue = "";
 		String solution = "";
 		String pattern = "??????????";
+		// Expected HTML status code
 		int statusCode = HttpServletResponse.SC_BAD_REQUEST;
-		String errorMessage = "Please enter a clue to solve.";
+		// Expected output message
+		String message = "Please enter a clue to solve.";
 
-		// Run the bad request test
-		inputBadRequest(clue, solution, pattern, statusCode, errorMessage);
+		// Run the test
+		doTest(clue, solution, pattern, statusCode, message);
 	}
 
 	/**
 	 * This method will test to ensure that the server does not try to process a
 	 * bad request. This method will attempt to post all required values, but
-	 * will ensure that the pattern format does not match the solution format.
+	 * will ensure that the pattern format does not match the solution format,
+	 * and will test for HTML, XML and JSON responses.
 	 */
 	@Test
 	public void testInvalidPattern() {
+		// Input Values
 		String clue = "Found ermine, deer hides damaged";
 		String solution = "10";
 		String pattern = "????????";
+		// Expected HTML status code
 		int statusCode = HttpServletResponse.SC_BAD_REQUEST;
-		String errorMessage = "Please ensure the solution format matches the pattern format.";
+		// Expected output message
+		String message = "Please ensure the solution format matches the pattern format.";
 
-		// Run the bad request test
-		inputBadRequest(clue, solution, pattern, statusCode, errorMessage);
+		// Run the test
+		doTest(clue, solution, pattern, statusCode, message);
 	}
 
 	/**
 	 * This method will test to ensure that the server does not try to process a
 	 * bad request. This method will attempt to post all required values, but
-	 * will ensure that the solution format does not match the pattern format.
+	 * will ensure that the solution format does not match the pattern format,
+	 * and will test for HTML, XML and JSON responses.
 	 */
 	@Test
 	public void testInvalidSolution() {
+		// Input Values
 		String clue = "Found ermine, deer hides damaged";
 		String solution = "5";
 		String pattern = "??????????";
+		// Expected HTML status code
 		int statusCode = HttpServletResponse.SC_BAD_REQUEST;
-		String errorMessage = "Please ensure the solution format matches the pattern format.";
+		// Expected output message
+		String message = "Please ensure the solution format matches the pattern format.";
 
-		// Run the bad request test
-		inputBadRequest(clue, solution, pattern, statusCode, errorMessage);
+		// Run the test
+		doTest(clue, solution, pattern, statusCode, message);
+	}
+
+	/**
+	 * This test helper method will run a test with the given test data clue,
+	 * solution and pattern.The method will make a request that expects HTML,
+	 * XML and JSON return types, and will validate the response against the
+	 * given HTML status code and the message that should be found within the
+	 * response.
+	 * 
+	 * @param clue
+	 *            the clue to be validated
+	 * @param solution
+	 *            the solution format to be validated
+	 * @param pattern
+	 *            the solution pattern format to be validated
+	 * @param statusCode
+	 *            the expected HTML status code
+	 * @param message
+	 *            the expected error message
+	 */
+	private void doTest(String clue, String solution, String pattern,
+			int statusCode, String message) {
+		// POST & GET Response
+		CloseableHttpResponse response;
+
+		// Test HTML POST request
+		response = getServerResponse(clue, solution, pattern, HttpVerb.POST,
+				HttpResponse.HTML);
+		testHtmlResponse(response, HttpVerb.POST, 200, message);
+		response = null;
+
+		// Test XML POST request
+		response = getServerResponse(clue, solution, pattern, HttpVerb.POST,
+				HttpResponse.XML);
+		testXmlResponse(response, HttpVerb.POST, statusCode, message);
+		response = null;
+
+		// Test JSON POST request
+		response = getServerResponse(clue, solution, pattern, HttpVerb.POST,
+				HttpResponse.JSON);
+		testJsonResponse(response, HttpVerb.POST, statusCode, message);
+		response = null;
+
+		// Test HTML GET request
+		response = getServerResponse(clue, solution, pattern, HttpVerb.GET,
+				HttpResponse.HTML);
+		testHtmlResponse(response, HttpVerb.GET, HttpServletResponse.SC_OK,
+				message);
+		response = null;
+
+		// Test XML GET request
+		response = getServerResponse(clue, solution, pattern, HttpVerb.GET,
+				HttpResponse.XML);
+		testXmlResponse(response, HttpVerb.GET, statusCode, message);
+		response = null;
+
+		// Test JSON GET request
+		response = getServerResponse(clue, solution, pattern, HttpVerb.GET,
+				HttpResponse.JSON);
+		testJsonResponse(response, HttpVerb.GET, statusCode, message);
+
+		// Close the response
+		try {
+			if (response != null) {
+				response.close();
+			}
+		} catch (IOException e) {
+			// Something has REALLY gone wrong.
+			fail("Test aborted due to unrecoverable error: " + e.getMessage());
+			response = null;
+		}
+	}
+
+	/**
+	 * This method will validate a HTML response against the status code and the
+	 * given message.
+	 * 
+	 * @param response
+	 *            the HTTP response object to be validated
+	 * @param action
+	 *            the type of request that was made
+	 * @param status
+	 *            the HTML status code expected to be found as part of the
+	 *            response
+	 * @param message
+	 *            the expected message to be found within the response
+	 */
+	private void testHtmlResponse(CloseableHttpResponse response,
+			HttpVerb action, int status, String message) {
+		// Obtain the server's HTML response
+		String html = getServerOutput(response);
+
+		// Ensure the response is HTML
+		String contentType = response.getEntity().getContentType().getValue();
+		assertTrue(contentType.contains("text/html"));
+
+		// Ensure the html has the required html tags
+		assertTrue(html.contains("<!DOCTYPE html><html lang=\"en\">"));
+		assertTrue(html.contains("</html>"));
+
+		// Ensure the html has the required head tags
+		assertTrue(html.contains("<head>"));
+		assertTrue(html.contains("</head>"));
+
+		// Ensure the html has the required head tags
+		assertTrue(html.contains("<body>"));
+		assertTrue(html.contains("</body>"));
+
+		// Ensure the html has cryptic title
+		assertTrue(html.contains("<title>Cryptic Crossword Solver</title>"));
+
+		// Ensure the expected status code is returned
+		int statusCode = response.getStatusLine().getStatusCode();
+		assertEquals(status, statusCode);
+
+		// Ensure the HTML message displays expected message
+		assertTrue(html.contains(message));
+	}
+
+	/**
+	 * This method will validate an XML response against the status code and the
+	 * given message.
+	 * 
+	 * @param response
+	 *            the HTTP response object to be validated
+	 * @param action
+	 *            the type of request that was made
+	 * @param status
+	 *            the HTML status code expected to be found as part of the
+	 *            response
+	 * @param message
+	 *            the expected message to be found within the response
+	 */
+	private void testXmlResponse(CloseableHttpResponse response,
+			HttpVerb action, int status, String message) {
+		// Obtain the server's XML response
+		String xml = getServerOutput(response);
+
+		// Ensure the response is XML
+		String contentType = response.getEntity().getContentType().getValue();
+		assertEquals("application/xml", contentType);
+
+		// Ensure the XML response has a root tag
+		assertTrue(xml.contains("<solver>"));
+		assertTrue(xml.contains("</solver>"));
+
+		// Ensure the expected status code is returned
+		int statusCode = response.getStatusLine().getStatusCode();
+		assertEquals(status, statusCode);
+
+		// Ensure the XML message displays expected message
+		assertTrue(xml.contains(message));
+	}
+
+	/**
+	 * This method will validate a JSON response against the status code and the
+	 * given message.
+	 * 
+	 * @param response
+	 *            the HTTP response object to be validated
+	 * @param action
+	 *            the type of request that was made
+	 * @param status
+	 *            the HTML status code expected to be found as part of the
+	 *            response
+	 * @param message
+	 *            the expected message to be found within the response
+	 */
+	private void testJsonResponse(CloseableHttpResponse response,
+			HttpVerb action, int status, String message) {
+		// Obtain the server's JSON response
+		String json = getServerOutput(response);
+
+		// Ensure the response is JSON
+		String contentType = response.getEntity().getContentType().getValue();
+		assertEquals("application/json", contentType);
+
+		// Ensure JSON is well formed
+		try {
+			new JSONObject(json);
+		} catch (JSONException e) {
+			fail("JSON String parse error: " + e.getMessage());
+		}
+
+		// Ensure the expected status code is returned
+		int statusCode = response.getStatusLine().getStatusCode();
+		assertEquals(status, statusCode);
+
+		// Ensure the JSON message displays expected message
+		assertTrue(json.contains(message));
 	}
 
 	/**
@@ -239,89 +474,65 @@ public class SolverTest {
 	 * @param error
 	 *            the expected error message
 	 */
-	private void inputBadRequest(String clue, String solution, String pattern,
-			int statusCode, String error) {
+	private CloseableHttpResponse getServerResponse(String clue,
+			String solution, String pattern, HttpVerb action,
+			HttpResponse returnType) {
+
 		// Formulate a new URI based upon required parameters
 		URI uri = getSolverURI(clue, solution, pattern);
 
 		// Obtain the response from the given URL
-		CloseableHttpResponse response = getHttpResponse(uri);
-
-		// Get the HTML returned from the server
-		String html = getServerOutput(response);
-
-		// Get the returned HTML status code
-		int htmlCode = response.getStatusLine().getStatusCode();
-
-		// Test the returned data
-		try {
-			// Ensure valid HTML was returned
-			assertTrue(validHtml(html));
-
-			// Ensure the HTML message contains the status code
-			assertTrue(html.contains("HTTP Status " + statusCode));
-
-			// Ensure the HTML message displays the correct message
-			assertTrue(html.contains(error));
-
-			// Ensure the server set the correct HTML status code
-			assertEquals(statusCode, htmlCode);
-		} finally {
-			// Close the response
-			try {
-				response.close();
-			} catch (IOException e) {
-				// Something has gone wrong.
-				fail("HTTP response was not closed: " + e.getMessage());
-			}
-		}
+		return getHttpResponse(uri, action, returnType);
 	}
 
 	/**
-	 * This method will decide if a given HTML string can be deemed as being
-	 * valid or not
-	 * 
-	 * @param html
-	 *            the HTML String to be analysed
-	 * @return True if the HTML is valid
-	 */
-	private boolean validHtml(String html) {
-		// HTML
-		if (!html.contains("<html>") || !html.contains("</html>")) {
-			return false;
-		}
-
-		// HEAD
-		if (!html.contains("<head>") || !html.contains("</head>")) {
-			return false;
-		}
-
-		// BODY
-		if (!html.contains("<body>") || !html.contains("</body>")) {
-			return false;
-		}
-
-		return true;
-	}
-
-	/**
-	 * This method will execute the given URI as a new POST to the client
+	 * This method will execute the given URI as the given action by the default
+	 * test client.
 	 * 
 	 * @param uri
 	 *            the URI to be executed
+	 * @param action
+	 *            the HTTP action to be performed
+	 * @param returnType
+	 *            the expected return type
 	 * @return The server response object
 	 */
-	private CloseableHttpResponse getHttpResponse(URI uri) {
+	private CloseableHttpResponse getHttpResponse(URI uri, HttpVerb action,
+			HttpResponse returnType) {
 		// HTTP Response object
 		CloseableHttpResponse response = null;
 
 		try {
-			// Create a new HTTP POST based upon the URI
-			HttpPost httppost = new HttpPost(uri);
+			// The HTTP request to be made
+			HttpRequestBase request = null;
+
+			// Cast the given action to the HTTP request
+			switch (action) {
+				case GET:
+					request = new HttpGet(uri);
+					break;
+
+				case POST:
+					request = new HttpPost(uri);
+					break;
+
+				default:
+					fail("An error has occured: " + action.toString()
+							+ " is not supported");
+			}
+
+			// Add additional headers if required (XML/JSON only)
+			if (!returnType.equals(HttpResponse.HTML)) {
+				// Add the Accept Header
+				String contentType = String.format("application/%s", returnType
+						.name().toLowerCase());
+				request.addHeader(HttpHeaders.ACCEPT, contentType);
+				// Set the AJAX header
+				request.addHeader("x-requested-with", "xmlhttprequest");
+			}
 
 			// Execute the HTTP POST request
-			response = client.execute(httppost);
-
+			response = client.execute(request);
 		} catch (IOException e) {
 			// Something has REALLY gone wrong.
 			fail("Test aborted due to unrecoverable error: " + e.getMessage());
