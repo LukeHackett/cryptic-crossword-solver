a84b6029eb0ca393ff61e9f98436a2604e35f12c
Stuart Leader <slug56@gmail.com>
2014-02-21 10:28:10 +0000

    Faster pronunciation lookup in Homophone Dictionary

--- webservice/src/uk/ac/hud/cryptic/resource/HomophoneDictionary.java
+++ webservice/src/uk/ac/hud/cryptic/resource/HomophoneDictionary.java
@@ -11,6 +11,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Set;
 
 import uk.ac.hud.cryptic.config.Settings;
@@ -34,6 +35,8 @@ public class HomophoneDictionary {
 	private static final String COMMENT = ";;;";
 	// Actual homophone dictionary data structure
 	private Map<String, List<String>> dictionary;
+	// Reverse map for fast lookup of pronunciations
+	private Map<List<String>, List<String>> reverseDictionary;
 	// Cache of homonyms
 	private Cache<String, Set<String>> cache;
 
@@ -76,8 +79,6 @@ public class HomophoneDictionary {
 				if (components.length == 2) {
 					// Normalise the lookup word
 					String word = components[0].toLowerCase().trim();
-					// Remove duplicate indicator from word
-					word = word.replaceAll("(\\d+)", "");
 					// Create a list of the pronunciation elements
 					List<String> pronunciation = Arrays.asList(components[1]
 							.split(WordUtils.REGEX_WHITESPACE));
@@ -86,9 +87,37 @@ public class HomophoneDictionary {
 				}
 			} while ((line = br.readLine()) != null);
 
+			// Effectively swap round the keys / values
+			populateReverseDictionary();
+
 		} catch (IOException e) {
 			System.err
 					.println("Exception in Homophone Dictionary initialisation.");
+		}
+	}
+
+	/**
+	 * This allows pronunciations to be looked up faster. For example, looking
+	 * up the pronunciation "HH AH0 L OW1" will return "hello". This saves
+	 * having to iterate the entire homophone map to search for words with the
+	 * same pronunciation.
+	 */
+	private void populateReverseDictionary() {
+		reverseDictionary = new HashMap<>();
+		// For each word-pronunciation pair in the existing map
+		for (Entry<String, List<String>> e : dictionary.entrySet()) {
+			// The value (pronunciation) now becomes the key
+			List<String> key = e.getValue();
+			// and the key (the word itself) now becomes the value
+			String value = e.getKey().replaceAll("\\(\\d+\\)", "");
+			// Note that a pronunciation can have multiple words attached to it
+			if (reverseDictionary.containsKey(key)) {
+				reverseDictionary.get(key).add(value);
+			} else {
+				List<String> words = new ArrayList<>();
+				words.add(value);
+				reverseDictionary.put(key, words);
+			}
 		}
 	}
 
@@ -131,6 +160,15 @@ public class HomophoneDictionary {
 		return pronunciations;
 	}
 
+	/**
+	 * Get words which share the same pronunciation as the supplied word. This
+	 * only works for words which share the exact same pronunciation.
+	 * 
+	 * @param word
+	 *            - the word to find the homonyms of
+	 * @return a set of words which share the same pronunciation (homonyms) as
+	 *         the supplied word
+	 */
 	public Set<String> getHomonyms(String word) {
 		// First check the cache
 		if (cache.containsKey(word)) {
@@ -142,14 +180,8 @@ public class HomophoneDictionary {
 		Collection<List<String>> pronunciations = getPronunciations(word);
 		// For each of these pronunciations
 		for (List<String> pronunciation : pronunciations) {
-			// Find matching words - iterate entire dictionary
-			for (String entry : dictionary.keySet()) {
-				// If pronunciations exactly match
-				if (getPronunciations(entry).contains(pronunciation)) {
-					// Add as a homonym
-					homonyms.add(entry);
-					// System.out.println(entry);
-				}
+			if (reverseDictionary.containsKey(pronunciation)) {
+				homonyms.addAll(reverseDictionary.get(pronunciation));
 			}
 			// Remove the original word, if present
 			homonyms.remove(word);
@@ -158,10 +190,5 @@ public class HomophoneDictionary {
 		}
 		return homonyms;
 	}
-
-	/**
-	 * Reverse lookup. Take the given pronunciation and find words which match
-	 * this.
-	 */
 
 } // End of class HomophoneDictionary
