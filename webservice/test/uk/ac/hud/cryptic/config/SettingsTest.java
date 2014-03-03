c8d03f8df306c9ac10782916d8dd8bcc6aafb40c
Mohammad Rahman <Mohammadsrahman@me.com>
2014-03-03 13:20:20 +0000

    Updated for 2nd Iteration

--- webservice/test/uk/ac/hud/cryptic/config/SettingsTest.java
+++ webservice/test/uk/ac/hud/cryptic/config/SettingsTest.java
@@ -1,16 +1,17 @@
 package uk.ac.hud.cryptic.config;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNotSame;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
-
 import java.io.IOException;
 import java.io.InputStream;
-
+import java.net.URL;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import uk.ac.hud.cryptic.config.Settings.ResourceType;
 
 /**
  * This test class provides a testing interface to the Settings class. The tests
@@ -18,8 +19,8 @@ import org.junit.Test;
  * are corrected stored, as well as ensuring that input streams can be obtained
  * for the various files.
  * 
- * @author Luke Hackett
- * @version 0.1
+ * @author Luke Hackett, Mohammad Rahman
+ * @version 0.2
  */
 public class SettingsTest {
 	// Settings instance under test within this unit test
@@ -47,7 +48,7 @@ public class SettingsTest {
 	 * that data can be read.
 	 */
 	@Test
-	public void testDictionaryPath() {
+	public void testGetDictionarStream() {
 		testStream(settings.getDictionaryStream());
 	}
 
@@ -56,7 +57,7 @@ public class SettingsTest {
 	 * and that data can be read.
 	 */
 	@Test
-	public void testGetCustomDictionaryPath() {
+	public void testGetCustomDictionaryStream() {
 		testStream(settings.getCustomDictionaryStream());
 	}
 
@@ -65,16 +66,79 @@ public class SettingsTest {
 	 * returned, and that data can be read.
 	 */
 	@Test
-	public void testGetDictionaryExclusionsPath() {
+	public void testGetDictionaryExclusionsStream() {
 		testStream(settings.getDictionaryExclusionsStream());
 	}
+	
+	/**
+	 * This test will ensure that a valid properties InputStream is
+	 * returned, and that data can be read.
+	 */
+	@Test
+	public void testGetPropertyStream() {
+		testStream(settings.getPropertyStream());
+	}
+	
+	/**
+	 * This test will ensure that a valid natural language processing POS
+	 * tagging model InputStream is returned, and that data can be read.
+	 */
+	@Test
+	public void testGetPOSModelStream() {
+		testStream(settings.getPOSModelStream());
+	}
+	
+	/**
+	 * This test will ensure that a valid Chunker model 
+	 * InputStream is returned, and that data can be read.
+	 */
+	@Test
+	public void testGetChunkerModelStream() {
+		testStream(settings.getChunkerModelStream());
+	}
+	
+	/**
+	 * This test will ensure that a valid Categoriser model 
+	 * InputStream is returned, and that data can be read.
+	 */
+	@Test
+	public void testGetCategoriserModelStream() {
+		testStream(settings.getCategoriserModelStream());
+	}
 
+	/**
+	 * This test will ensure that a valid Sentence Detector model 
+	 * InputStream is returned, and that data can be read.
+	 */
+	@Test
+	public void testGetSentenceDetectorModelStream() {
+		testStream(settings.getSentenceDetectorModelStream());
+	}
+	
+	/**
+	 * This test will ensure that a valid Parser model 
+	 * InputStream is returned, and that data can be read.
+	 */
+	@Test
+	public void testGetParserModelStream() {
+		testStream(settings.getParserModelStream());
+	}
+	
+	/**
+	 * This test will ensure that a valid Tokeniser model 
+	 * InputStream is returned, and that data can be read.
+	 */
+	@Test
+	public void testGetTokeniserModelStream() {
+		testStream(settings.getTokeniserModelStream());
+	}
+	
 	/**
 	 * This test will ensure that a valid thesaurus InputStream is returned, and
 	 * that data can be read.
 	 */
 	@Test
-	public void testGetThesaurusPath() {
+	public void testGetThesaurusStream() {
 		testStream(settings.getThesaurusStream());
 	}
 
@@ -83,7 +147,7 @@ public class SettingsTest {
 	 * returned, and that data can be read.
 	 */
 	@Test
-	public void testGetPronouncingDictionaryPath() {
+	public void testGetHomophoneDictionaryStream() {
 		testStream(settings.getHomophoneDictionaryStream());
 	}
 
@@ -119,7 +183,27 @@ public class SettingsTest {
 	public void testGetDBPassword() {
 		assertEquals("du4hacrEKa", settings.getDBPassword());
 	}
+	
+	/**
+	 * This method will test the returned value of a filename does 
+	 * not contain any extensions.
+	 */
+	@Test
+	public void testGetFilename() {
+		String test = settings.getFileName("anagrams.txt", "txt");
+		assertEquals(test, "anagrams");
+	}
 
+	/**
+	 * This method will test a directory is retrieved from the 
+	 * the given classpath, represented as a URL.
+	 */
+	@Test
+	public void testGetIndicatorsURL() {
+		URL url = settings.getIndicatorsURL(ResourceType.ASSET, "indicators");
+		assertNotNull(url);
+	}
+		
 	/**
 	 * This method provides a generic interface to test to ensure that a given
 	 * InputStrem is able to "contact" the associated file.
