0649e8b60328f8651a1dbf4d5730da5a1fb7ed8f
Stuart Leader <slug56@gmail.com>
2014-03-04 19:21:06 +0000

    Add CacheTest to UtilSuite

--- webservice/test/uk/ac/hud/cryptic/util/UtilSuite.java
+++ webservice/test/uk/ac/hud/cryptic/util/UtilSuite.java
@@ -14,7 +14,8 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 
 @RunWith(value = Suite.class)
-@SuiteClasses(value = { DBTest.class, WordUtilsTest.class, ConfidenceTest.class })
+@SuiteClasses(value = { DBTest.class, WordUtilsTest.class,
+		ConfidenceTest.class, CacheTest.class })
 public class UtilSuite {
 
 }
