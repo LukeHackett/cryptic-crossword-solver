dad98686efff92983f968c53356c7da304a25cfa
Mohammad Rahman <Mohammadsrahman@me.com>
2014-03-04 13:28:09 +0000

    Updated test for Iteration 2

--- webservice/test/uk/ac/hud/cryptic/resource/HomophoneDictionaryTest.java
+++ webservice/test/uk/ac/hud/cryptic/resource/HomophoneDictionaryTest.java
@@ -1,20 +1,75 @@
 package uk.ac.hud.cryptic.resource;
 
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 
+
 public class HomophoneDictionaryTest {
-	@Test
-	public void testGetPronunciations() {
+	
+	private static ArrayList<String> words = new ArrayList<String>();
+	private static ArrayList<String> results = new ArrayList<String>();
+	private static HomophoneDictionary dictionary;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
 		
+	dictionary = HomophoneDictionary.getInstance();
+		
+	 words.add("groan");
+	 words.add("hair");
+	 words.add("hall");
+	 words.add("hear");
+	 words.add("plain");
+	 words.add("rain");
+	 words.add("write");
+	 words.add("wait");
+	 words.add("which");
+	 words.add("knew");
+	 
+	 results.add("grown");
+	 results.add("hare");
+	 results.add("haul");
+	 results.add("here");
+	 results.add("plane");
+	 results.add("reign");
+	 results.add("right");
+	 results.add("weight");
+	 results.add("witch");
+	 results.add("new");
+	 
 	}
 	
+	
+	/**
+	 * This test ensures that a pattern for pronunciation for each word
+	 * is found to be later used to find the Homonyms.
+	 */
+	@Test
+	public void testGetPronunciations() {
+		for(String word: words){
+			assertNotNull(dictionary.getPronunciations(word));
+		}
+	}
+	
+	
+	/**
+	 * This test checks that for all the provided test words
+	 * it finds and populates some homonyms. The test then ensures that the
+	 * correct homonym is in the results.
+	 */
 	@Test
 	public void testGetHomonyms() {
 		
-	}
-	
-	@Test
-	public void testReverseDictionary() {
+		for(int i =0; i < words.size(); i++ ){
+			Set<String> matches = dictionary.getHomonyms(words.get(i));
+			assertTrue( matches.contains(results.get(i)));		
+		}
 		
 	}
+	
 }
