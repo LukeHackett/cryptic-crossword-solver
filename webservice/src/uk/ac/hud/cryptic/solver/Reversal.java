595e56c937c67ca27685bcace8a6dad604a83707
Luke Hackett <luke.hackett@live.co.uk>
2014-03-07 15:17:09 +0000

    First commit of the Reversal algorthim riding on NLP

--- webservice/src/uk/ac/hud/cryptic/solver/Reversal.java
+++ webservice/src/uk/ac/hud/cryptic/solver/Reversal.java
@@ -1,9 +1,16 @@
 package uk.ac.hud.cryptic.solver;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
+
+import opennlp.tools.postag.POSModel;
+import opennlp.tools.postag.POSTaggerME;
 
 import uk.ac.hud.cryptic.resource.Thesaurus;
 import uk.ac.hud.cryptic.solver.Solver;
+import uk.ac.hud.cryptic.config.Settings;
 import uk.ac.hud.cryptic.core.Clue;
 import uk.ac.hud.cryptic.core.Solution;
 import uk.ac.hud.cryptic.core.SolutionCollection;
@@ -11,18 +18,17 @@ import uk.ac.hud.cryptic.core.SolutionPattern;
 import uk.ac.hud.cryptic.util.WordUtils;
 
 /**
- * Reversal solver algorithm, currently does not support NLP.
+ * Reversal solver algorithm that utilises a small amount of NLP to cut down the
+ * search space when solving clue.
  * 
  * @author Luke Hackett
- * @version 0.2
+ * @version 0.3
  */
 public class Reversal extends Solver {
-
 	// A readable (and DB-valid) name for the solver
 	private static final String NAME = "reversal";
-
-	// TODO once NLP has been added this can be removed
-	private static String FODDER;
+	// Settings instance
+	private Settings settings = Settings.getInstance();
 
 	/**
 	 * Default constructor for the reversal solver class
@@ -51,16 +57,6 @@ public class Reversal extends Solver {
 	 */
 	@Override
 	public SolutionCollection solve(Clue c) {
-		// TODO NLP
-		// NLP would be used here to obtain the definition, fodder and
-		// indicator. At this point only the fodder is really used, but
-		// the definition could be used to but the fodder into perspective
-		// e.g. definition character, therefore Beta as an answer gets it's
-		// confidence rating boosted
-
-		// Get the Fodder
-		String fodder = FODDER;
-
 		// Contains all the possible solutions to the clue
 		SolutionCollection collection = new SolutionCollection();
 
@@ -69,20 +65,27 @@ public class Reversal extends Solver {
 				.getPattern());
 		SolutionPattern pattern = new SolutionPattern(reversedPattern);
 
-		// Get all synonyms that match the reversed pattern
-		Set<String> synonyms = THESAURUS.getMatchingSynonyms(fodder, pattern);
+		// Get the possible fodders
+		String[] fodders = getFodders(c);
 
-		// Add the fodder as a synonym as it could contain the answer
-		synonyms.add(fodder);
+		// Loop over all calculated fodders
+		for (String fodder : fodders) {
+			// Get all synonyms that match the reversed pattern
+			Set<String> synonyms = THESAURUS.getSecondSynonyms(fodder, pattern,
+					true);
 
-		// Reverse all synonyms to try to create another word
-		for (String synonym : synonyms) {
-			// Reverse the String
-			String reversedWord = WordUtils.reverseWord(synonym);
+			// Add the fodder as a synonym as it could contain the answer
+			synonyms.add(fodder);
 
-			// Add as a solution if the reversed word is a real word
-			if (DICTIONARY.isWord(reversedWord)) {
-				collection.add(new Solution(reversedWord, NAME));
+			// Reverse all synonyms to try to create another word
+			for (String synonym : synonyms) {
+				// Reverse the String
+				String reversedWord = WordUtils.reverseWord(synonym);
+
+				// Add as a solution if the reversed word is a real word
+				if (DICTIONARY.isWord(reversedWord)) {
+					collection.add(new Solution(reversedWord, NAME));
+				}
 			}
 		}
 
@@ -90,6 +93,47 @@ public class Reversal extends Solver {
 		Thesaurus.getInstance().confidenceAdjust(c, collection);
 
 		return collection;
+	}
+
+	/**
+	 * This method will return an array of possible Fodders based upon the given
+	 * clue. A reversal fodder is normally a singular or plural noun, but may be
+	 * a number of words.
+	 * 
+	 * @param c
+	 *            - the clue to solve
+	 * @return An array of possible fodders
+	 */
+	private String[] getFodders(Clue clue) {
+		// A list of possible fodders
+		List<String> fodders = new ArrayList<String>();
+
+		try {
+			// Create a new POSModel
+			POSModel model = new POSModel(settings.getPOSModelStream());
+
+			// Apply to the tagger
+			POSTaggerME tagger = new POSTaggerME(model);
+
+			// Split on all whitespace
+			String words[] = WordUtils.getWords(clue.getClue());
+
+			// Get a list of the tags
+			String[] tags = tagger.tag(words);
+
+			// This is a guess...
+			for (int i = 0; i < tags.length; i++) {
+				// ...Fodders are marked with 'NN' or 'NNS'... usually
+				if (tags[i].equals("NN") || tags[i].equals("NNS")) {
+					fodders.add(words[i]);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Return the fodders as a String array
+		return fodders.toArray(new String[fodders.size()]);
 	}
 
 	/**
@@ -106,26 +150,7 @@ public class Reversal extends Solver {
 	 * Entry point to the code for testing purposes
 	 */
 	public static void main(String[] args) {
-
-		Clue clue = new Clue("Put back trap item", "p???");
-		FODDER = "trap";
-
-		// Clue clue = new Clue("Sketcher went up to get reward", "??????");
-		// FODDER = "reward";
-
-		// Clue clue = new Clue("Devil from the east existed", "?????");
-		// FODDER = "devil";
-
-		// Solve the clue
-		Reversal reversal = new Reversal(clue);
-		SolutionCollection solutions = reversal.solve(clue);
-
-		// Output the results
-		System.out.println("Found " + solutions.size() + " solutions");
-		for (Solution s : solutions) {
-			System.out.println(s.getSolution());
-		}
-
+		testSolver(Reversal.class);
 	}
 
 }
